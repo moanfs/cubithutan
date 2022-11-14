b2de9dd7dece601001fab1a09800646a27d2323f
coboi <ajiex018@gmail.com>
2022-11-14 12:34:51 +0700

    add index route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,7 +35,9 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+$routes->group('', ['namespace' => 'App\Controllers\User'], function ($routes) {
+    $routes->get('/', 'Dashboard::index');
+});
 /*
  * --------------------------------------------------------------------
  * Additional Routing
---
+++ app/Controllers/User/Dashboard.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers\User;
+
+use App\Controllers\BaseController;
+
+class Dashboard extends BaseController
+{
+    public function index()
+    {
+        return view('user/index');
+    }
+}
+
+?>
