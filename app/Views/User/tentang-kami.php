5846648656f7313146b42a89454753333f303e1f
coboi <ajiex018@gmail.com>
2022-11-18 09:53:25 +0700

    add about

--- app/Views/User/tentang-kami.php
+++ app/Views/User/tentang-kami.php
@@ -1,1 +1,53 @@
-tentang
+<!-- halaman tentang kami -->
+
+<?= $this->extend('layout/user-layout') ?>
+
+<?= $this->section('content') ?>
+
+<main>
+	<div class="container">
+		<div class="about-page">
+			<h1>Apa sih Cubit Hutan itu?</h1>
+			<p>Cubit Hutan adalah lorem ipsum dolor sit amet consectetur. Libero malesuada sollicitudin eget dignissim urna. Est posuere pellentesque sit sed. Condimentum eros nunc sapien molestie adipiscing tincidunt enim enim. Interdum nunc neque viverra euismod in feugiat iaculis vitae nunc. Quis metus ipsum habitant sed et pulvinar tortor nisl et. Ultrices mauris aliquet elit aliquam lacinia dis egestas. Risus suscipit porta senectus sed. Posuere lorem sem aliquam sed netus arcu sit pharetra.</p>
+			<h1>Siapa saja penggagas Cubit Hutan?</h1>
+			<div class="about-team">
+				<div class="row">
+					<div class="col-12 col-md-6">
+						<div class="member">
+							<h2>Devi Handayani</h2>
+							<p>Mahasiswa Lorem ipsum</p>
+							<p>Universitas Lorem ipsum</p>
+							<p class="profile">Cubit Hutan adalah lorem ipsum dolor sit amet consectetur. Libero malesuada sollicitudin eget dignissim urna.</p>
+						</div>
+					</div>
+					<div class="col-12 col-md-6">
+						<div class="member">
+							<h2>Devi Handayani</h2>
+							<p>Mahasiswa Lorem ipsum</p>
+							<p>Universitas Lorem ipsum</p>
+							<p class="profile">Cubit Hutan adalah lorem ipsum dolor sit amet consectetur. Libero malesuada sollicitudin eget dignissim urna.</p>
+						</div>
+					</div>
+					<div class="col-12 col-md-6">
+						<div class="member">
+							<h2>Devi Handayani</h2>
+							<p>Mahasiswa Lorem ipsum</p>
+							<p>Universitas Lorem ipsum</p>
+							<p class="profile">Cubit Hutan adalah lorem ipsum dolor sit amet consectetur. Libero malesuada sollicitudin eget dignissim urna.</p>
+						</div>
+					</div>
+					<div class="col-12 col-md-6">
+						<div class="member">
+							<h2>Devi Handayani</h2>
+							<p>Mahasiswa Lorem ipsum</p>
+							<p>Universitas Lorem ipsum</p>
+							<p class="profile">Cubit Hutan adalah lorem ipsum dolor sit amet consectetur. Libero malesuada sollicitudin eget dignissim urna.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
+
+<?= $this->endSection() ?>
