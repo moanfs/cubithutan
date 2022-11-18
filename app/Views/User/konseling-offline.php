b0aeff3f32cf91f0eac0cce5d7c24d0f6af8ad08
coboi <ajiex018@gmail.com>
2022-11-18 08:15:27 +0700

    add konseling offline

--- app/Views/User/konseling-offline.php
+++ app/Views/User/konseling-offline.php
@@ -1,1 +1,125 @@
-offline
+<!-- halaman konseling individual offline -->
+
+<?= $this->extend('layout/user-layout') ?>
+
+<?= $this->section('content') ?>
+
+<main>
+	<div class="container">
+		<div class="konseling-page">
+			<div class="row">
+				<div class="col-12 col-md-6">
+					<div class="konseling-topics">
+						<p>Pilih Topik Konseling</p>
+						<div class="row">
+							<div class="col-3">
+								<a class="konseling-item" href="#">
+									<i class="bi bi-emoji-neutral"></i>
+									<p>Kecemasan</p>
+								</a>
+							</div>
+							<div class="col-3">
+								<a class="konseling-item" href="#">
+									<i class="bi bi-emoji-neutral"></i>
+									<p>Kecemasan</p>
+								</a>
+							</div>
+							<div class="col-3">
+								<a class="konseling-item" href="#">
+									<i class="bi bi-emoji-neutral"></i>
+									<p>Kecemasan</p>
+								</a>
+							</div>
+							<div class="col-3">
+								<a class="konseling-item" href="#">
+									<i class="bi bi-emoji-neutral"></i>
+									<p>Kecemasan</p>
+								</a>
+							</div>
+							<div class="col-3">
+								<a class="konseling-item" href="#">
+									<i class="bi bi-emoji-neutral"></i>
+									<p>Kecemasan</p>
+								</a>
+							</div>
+							<div class="col-3">
+								<a class="konseling-item" href="#">
+									<i class="bi bi-emoji-neutral"></i>
+									<p>Kecemasan</p>
+								</a>
+							</div>
+							<div class="col-3">
+								<a class="konseling-item" href="#">
+									<i class="bi bi-emoji-neutral"></i>
+									<p>Kecemasan</p>
+								</a>
+							</div>
+							<div class="col-3">
+								<a class="konseling-item" href="#">
+									<i class="bi bi-emoji-neutral"></i>
+									<p>Kecemasan</p>
+								</a>
+							</div>
+							<div class="col-3">
+								<a class="konseling-item" href="#">
+									<i class="bi bi-emoji-neutral"></i>
+									<p>Kecemasan</p>
+								</a>
+							</div>
+							<div class="col-3">
+								<a class="konseling-item" href="#">
+									<i class="bi bi-emoji-neutral"></i>
+									<p>Kecemasan</p>
+								</a>
+							</div>
+							<div class="col-3">
+								<a class="konseling-item" href="#">
+									<i class="bi bi-emoji-neutral"></i>
+									<p>Kecemasan</p>
+								</a>
+							</div>
+							<div class="col-3">
+								<a class="konseling-item" href="#">
+									<i class="bi bi-emoji-neutral"></i>
+									<p>Kecemasan</p>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-md-6">
+					<div class="konseling-text">
+						<h1>Konseling Individual Offline</h1>
+						<p>Lorem ipsum dolor sit amet consectetur. Id massa amet egestas scelerisque. Aliquet purus odio aliquet neque arcu ullamcorper ipsum. Tellus arcu posuere quis faucibus a egestas cursus enim euismod. Volutpat sit nec nulla est tincidunt habitant orci praesent. Lobortis senectus cras in tincidunt aliquam faucibus elementum aliquam. Sed non egestas nec sem ornare viverra pharetra a. Imperdiet lectus sit eget id. Ultricies purus sed hendrerit a volutpat est dui mauris.</p>
+						<p>Sit id turpis suspendisse convallis feugiat at porttitor. Nulla at enim pharetra habitant consequat nunc.</p>
+					</div>
+				</div>
+			</div>
+			<div class="konseling-tab">
+				<ul class="nav nav-pills" id="tabs">
+					<li class="nav-item">
+						<button class="nav-link active" id="tabTujuan" data-bs-toggle="pill" data-bs-target="#tujuan">Tujuan Konseling</button>
+					</li>
+					<li class="nav-item">
+						<button class="nav-link" id="tabPertanyaan" data-bs-toggle="pill" data-bs-target="#pertanyaan">Seputar Pertanyaan</button>
+					</li>
+				</ul>
+				<div class="tab-content" id="pillsContent">
+					<div class="tab-pane show active" id="tujuan" tabindex="0">
+						<h2>Tujuan Dari Konseling Individual Offline</h2>
+						<p>Ketika anda merasa bahwa hidup ini tidak lagi tenang secara emosional:</p>
+						<ul>
+							<li>Mudah gelisah</li>
+							<li>Marah berlebihan</li>
+							<li>Sering mimpi buruk</li>
+							<li>Ketakutan yang berlebihan</li>
+						</ul>
+					</div>
+					<div class="tab-pane" id="pertanyaan" tabindex="0">b</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
+
+<?= $this->endSection() ?>
