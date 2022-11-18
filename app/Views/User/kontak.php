6a3959f0eac79946915d17dfed68e5cdbbd6f6d9
coboi <ajiex018@gmail.com>
2022-11-18 10:15:22 +0700

    add contact

--- app/Views/User/kontak.php
+++ app/Views/User/kontak.php
@@ -1,1 +1,37 @@
-kontak
+<!-- halaman kontak -->
+
+<?= $this->extend('layout/user-layout') ?>
+
+<?= $this->section('content') ?>
+
+<main>
+	<div class="container">
+		<div class="contact-page">
+			<h1>Hubungi Kami</h1>
+			<p>Lorem ipsum dolor sit amet consectetur. Libero malesuada sollicitudin eget dignissim urna. Est posuere pellentesque sit sed. Condimentum eros nunc sapien molestie adipiscing tincidunt enim enim.</p>
+			<div class="contact-item">
+				<i class="bi bi-whatsapp"></i>
+				<span>081234567890</span>
+			</div>
+			<div class="contact-item">
+				<i class="bi bi-envelope-at"></i>
+				<span>cubit@hutan.com</span>
+			</div>
+			<h1>Media Sosial</h1>
+			<div class="contact-item">
+				<i class="bi bi-instagram"></i>
+				<span>@cubit_hutan</span>
+			</div>
+			<div class="contact-item">
+				<i class="bi bi-twitter"></i>
+				<span>@cubit_hutan</span>
+			</div>
+			<div class="contact-item">
+				<i class="bi bi-spotify"></i>
+				<span>Cubit Hutan</span>
+			</div>
+		</div>
+	</div>
+</main>
+
+<?= $this->endSection() ?>
