6cd1b2097f2cec26b52beb6cd1e25d8e2eaee294
coboi <ajiex018@gmail.com>
2022-12-03 09:36:19 +0700

    edit admin profile

--- app/Views/Admin/profile.php
+++ app/Views/Admin/profile.php
@@ -5,12 +5,135 @@
 <?= $this->section('content') ?>
 
 <!-- Begin Page Content -->
-<div class="container-fluid">
+<div class="container-fluid profile-page">
 
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800">Profile</h1>
 
+    <div class="row">
+        <div class="col-12 col-md-4">
+            <div class="card shadow">
+                <div class="card-body profile-img">
+                    <img src="<?= base_url('assets/img/avatar-lg.png') ?>" />
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-8">
+            <div class="card shadow">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-12 col-md-4">
+                            <p class="text-gray-900">Nama Panggilan</p>
+                        </div>
+                        <div class="col-12 col-md-8">
+                            <p>Malika</p>
+                        </div>
+
+                        <div class="col-12 col-md-4">
+                            <p class="text-gray-900">Email</p>
+                        </div>
+                        <div class="col-12 col-md-8">
+                            <span>malika@gmail.com</span>
+                            <span>
+                                <a href="#">Ganti email</a>
+                            </span>
+                        </div>
+
+                        <div class="col-12 col-md-4">
+                            <p class="text-gray-900">Kata Sandi</p>
+                        </div>
+                        <div class="col-12 col-md-8">
+                            <span>*********</span>
+                            <span>
+                                <a href="#">Ganti kata sandi</a>
+                            </span>
+                        </div>
+
+                        <div class="col-12 col-md-4">
+                            <p class="text-gray-900">Tempat, Tanggal Lahir</p>
+                        </div>
+                        <div class="col-12 col-md-8">
+                            <p>Bandung, 10 Januari 2000</p>
+                        </div>
+
+                        <div class="col-12 col-md-4">
+                            <p class="text-gray-900">Jenis Kelamin</p>
+                        </div>
+                        <div class="col-12 col-md-8">
+                            <p>Perempuan</p>
+                        </div>
+
+                        <div class="col-12 col-md-4">
+                            <p class="text-gray-900">No WhatsApp</p>
+                        </div>
+                        <div class="col-12 col-md-8">
+                            <p>08210808323</p>
+                        </div>
+
+                        <div class="col-12 col-md-4">
+                            <p class="text-gray-900">Alamat</p>
+                        </div>
+                        <div class="col-12 col-md-8">
+                            <p>Jl. Telekomunikasi. 1, Terusan Buahbatu - Bojongsoang, Telkom University, Sukapura, Kec. Dayeuhkolot, Kabupaten Bandung, Jawa Barat</p>
+                        </div>
+
+                        <div class="col-12">
+                            <button class="btn btn-primary" data-toggle="modal" data-target="#profileModal">Perbarui Profil</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
 <!-- /.container-fluid -->
+
+<!-- modal -->
+
+<div class="modal modal-profile" id="profileModal">
+    <div class="modal-dialog modal-dialog-centered modal-xl modal-fullscreen-sm-down">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-12 col-md-4">
+                        <div class="card">
+                            <!-- klik card untuk pilih upload -->
+                            <label class="card-body profile-img" for="uploadPhoto">
+                                <img src="<?= base_url('assets/img/avatar-lg.png') ?>" />
+                                <p>Besar file maksimum 10 MB.</p>
+                                <p>Format file JPG/JPEG/PNG.</p>
+                            </label>
+                            <input type="file" id="uploadPhoto" accept="image/jpg, image/jpeg, image/png" hidden />
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-8">
+                        <div class="modal-form">
+                            <h1>Perbarui Profil</h1>
+                            <form>
+                                <input class="form-control" type="text" placeholder="nama panggilan" />
+                                <input class="form-control" type="text" placeholder="tempat lahir" />
+                                <input class="form-control" type="text" placeholder="tanggal lahir" />
+                                <input class="form-control" type="text" placeholder="jenis kelamin" />
+                                <input class="form-control" type="text" placeholder="no whatsapp" />
+                                <textarea class="form-control" placeholder="alamat"></textarea>
+
+                                <div class="form-button">
+                                    <button class="btn btn-primary" type="submit">Simpan</button>
+                                    <button class="btn btn-danger" data-dismiss="modal">Batal</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?= $this->endSection() ?>
--- app/Views/Layout/admin-layout.php
+++ app/Views/Layout/admin-layout.php
@@ -11,13 +11,14 @@
 	<title>Cubit Hutan</title>
 
 	<!-- Custom fonts for this template-->
-	<link href="<?= base_url('assets/sbadmin/vendor/fontawesome-free/css/all.min.cs') ?>s" rel="stylesheet" type="text/css">
+	<link href="<?= base_url('assets/sbadmin/vendor/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
 	<link
 		href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
 		rel="stylesheet">
 
 	<!-- Custom styles for this template-->
 	<link href="<?= base_url('assets/sbadmin/css/sb-admin-2.min.css') ?>" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/style-admin.css') ?>">
 
 </head>
 
@@ -30,7 +31,7 @@
 		<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
 			<!-- Sidebar - Brand -->
-			<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+			<a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= site_url('/') ?>">
 				<div class="sidebar-brand-icon rotate-n-15">
 					<i class="fas fa-laugh-wink"></i>
 				</div>
