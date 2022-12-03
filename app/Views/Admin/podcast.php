e284e57b4f5fac00e00eecbf257a80279a5db246
coboi <ajiex018@gmail.com>
2022-12-03 10:30:02 +0700

    edit admin podcast

--- app/Views/Admin/podcast.php
+++ app/Views/Admin/podcast.php
@@ -5,11 +5,42 @@
 <?= $this->section('content') ?>
 
 <!-- Begin Page Content -->
-<div class="container-fluid">
+<div class="container-fluid podcast-page">
 
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800">Podcast</h1>
 
+    <div class="card shadow">
+        <div class="card-header py-3">
+            <button class="btn btn-primary">Tambah Episode</button>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>Episode</th>
+                            <th>Tanggal</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <iframe class="podcast-episode" src="https://open.spotify.com/embed/episode/6reBRsYux87Lt6xLSdkG0M?si=3284e9c8061845f4"></iframe>
+                            </td>
+                            <td>
+                                1/2/23
+                            </td>
+                            <td>
+                                <button class="btn btn-primary">Detail</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
 <!-- /.container-fluid -->
 
